3c4b2e1893c1d95d9ffed567f749bfd1405deac5
Fabio Moraes <fabim.nmoraes@gmail.com>
2021-09-08 20:27:30 -0300

    Atualizaçao de telas

--- src/modules/pub-tables/pub-tables.module.ts
+++ src/modules/pub-tables/pub-tables.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PubTablesService } from './pub-tables.service';
 import { PubTablesController } from './pub-tables.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PubTable } from './entities/pub-table.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PubTable])],
   controllers: [PubTablesController],
-  providers: [PubTablesService]
+  providers: [PubTablesService],
 })
 export class PubTablesModule {}
